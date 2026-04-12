ea2ef1f8edc0800353184da51cf184429abdc15f
furkanaslan71 <e2521219@ceng.metu.edu.tr>
2026-04-13 02:52:04 +0300

    done profile info crud

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,9 @@
 # app/api/router.py
 from fastapi import APIRouter
-from app.api.endpoints import auth, users
+from app.api.endpoints import auth, users, profiles
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(profiles.router, prefix="/profiles", tags=["profiles"])
